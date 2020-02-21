e8a18cda372578281bdb00c6469826cee3a90fd2
David Sargeant <david@dsargeant.com>
2020-02-21 16:42:15 -0500

    Tweaks.

--- greeting.process.js
+++ greeting.process.js
@@ -2,6 +2,6 @@ let Bluebird = require('bluebird')
 
 module.exports = async job => {
   console.log(job.data)
-  await Bluebird.delay(100)
+  await Bluebird.delay(job.data.name === 'Bob' ? 1000 : 100)
   return `Hello, ${job.data.name}`
 }
