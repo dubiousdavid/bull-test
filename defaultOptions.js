0c2a13e9113be8775421bfddfecb3c4ff271dd43
David Sargeant <david@dsargeant.com>
2020-02-25 11:28:46 -0500

    Tweaks.

--- defaultOptions.js
+++ defaultOptions.js
@@ -1,17 +1,19 @@
 module.exports = {
-  // Max 30 jobs per second
+  // These options need to be present when initializing the consumer side of the queue
   limiter: {
-    max: 30,
+    // Max jobs per second
+    max: 5,
     duration: 1000,
   },
+  // These options need to be present when initializing the producer side of the queue
   defaultJobOptions: {
     // Attempt up to 2 times.
     attempts: 2,
     // Timeout the job after 500ms
     timeout: 500,
     // Number of completed jobs to keep
-    // removeOnComplete: 500,
+    // removeOnComplete: 50,
     // Number of failed jobs to keep
-    // removeOnFail: 500
+    // removeOnFail: 5
   },
 }
--- plus2.producer.js
+++ plus2.producer.js
@@ -1,6 +1,7 @@
 let Queue = require('bull')
+let options = require('./defaultOptions')
 
-let queue = new Queue('Plus2')
+let queue = new Queue('Plus2', options)
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max))
@@ -9,7 +10,7 @@ function getRandomInt(max) {
 setTimeout(() => {
   queue.add({ number: 13 })
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     queue.add({ number: getRandomInt(1000) })
   }
 }, 500)
